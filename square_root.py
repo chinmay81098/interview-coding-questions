46455c39493b7cfb8400485878798842f68067c1
Chinmay Jain <chinmay81098@gmail.com>
2020-04-28 16:35:33 +0530

    trivial case fix

--- square_root.py
+++ square_root.py
@@ -1,7 +1,7 @@
 def squareRoot(f,l,n):
         m = (f+l)/2
         sq = m*m
-        if sq == n or abs(sq - n)< 0.0001:
+        if sq == n or abs(sq - n)< 0.000001:
             print("Square root: {0:.5f}".format(m))
         elif sq < n:
             squareRoot(m,l,n)
@@ -11,14 +11,18 @@ def find_square_root(n):
     curr = 1
     found = False
     while not found:
-        mul = curr*curr
-        if mul == n:
-            print("Square root: {}".format(curr))
+        if n == 0:
+            print("Square root: {}".format(0))
             found = True
-        elif mul > n:
-            squareRoot(curr-1,curr,n)
-            found = True
-        curr += 1
+        else:
+            mul = curr*curr
+            if mul == n:
+                print("Square root: {}".format(curr))
+                found = True
+            elif mul > n:
+                squareRoot(curr-1,curr,n)
+                found = True
+            curr += 1
 
 n = float(input("Enter a number :"))
 find_square_root(n)
