0115453fe94bbf508c7150ae3510821da8d1310a
Chinmay Jain <chinmay81098@gmail.com>
2020-04-29 23:49:55 +0530

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+def selectionSort(arr,n):
+    for i in range(n):
+        minn = i
+        for j in range(i+1,n):
+            if arr[j] < arr[minn]:
+                minn = j
+        temp = arr[i]
+        arr[i] = arr[minn]
+        arr[minn] = temp
+    return arr
+
+
+print("Enter your array")
+arr = list(map(int,input().split()))
+n = len(arr)
+selection_sorted_arr = selectionSort(arr,n)
+print(selection_sorted_arr)
