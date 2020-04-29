009316b71680fe29b7efc4df34700f8eb143574e
Chinmay Jain <chinmay81098@gmail.com>
2020-04-29 23:11:49 +0530

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr,n):
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+print("Enter your array")
+arr = list(map(int,input().split()))
+n = len(arr)
+sorted_array = bubbleSort(arr,n)
+print(sorted_array)
